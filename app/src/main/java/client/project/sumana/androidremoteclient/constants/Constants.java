6929316ad13db952d6519b866d25ed863fddc892
Sumana25 <sumana.1rn12cs111@gmail.com>
2016-04-24 17:24:22 +0530

    Added HTTPClient

---
+++ app/src/main/java/client/project/sumana/androidremoteclient/constants/Constants.java
@@ -0,0 +1,8 @@
+package client.project.sumana.androidremoteclient.constants;
+
+/**
+ * Created by Sumana on 24-04-2016.
+ */
+public class Constants {
+    public static String SERVER_ADDRESS = "";
+}
