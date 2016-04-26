0bfa5af26a95cbc00f024ab54cd70aa08ba8e509
Karthik A <akarthik101@gmail.com>
2016-04-27 00:13:41 +0530

    Fragments and basic stuff

---
+++ app/src/main/java/client/project/sumana/androidremoteclient/constants/Constants.java
@@ -0,0 +1,9 @@
+package client.project.sumana.androidremoteclient.constants;
+
+/**
+ * Created by Karthik A on 26-04-16.
+ */
+public class Constants {
+
+    public static String SERVER_ADDRESS = "http://192.168.1.6:8080";
+}
---
+++ app/src/main/java/client/project/sumana/androidremoteclient/voicefragment/VoiceFragment.java
@@ -0,0 +1,146 @@
+package client.project.sumana.androidremoteclient.voicefragment;
+
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import client.project.sumana.androidremoteclient.R;
+import client.project.sumana.androidremoteclient.http.HTTPRequestClient;
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VoiceFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VoiceFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ImageView voiceBtn;
+    public static final int REQ_CODE_SPEECH_INPUT = 0x1;
+
+    public VoiceFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VoiceFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VoiceFragment newInstance(String param1, String param2) {
+        VoiceFragment fragment = new VoiceFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_voice, container, false);
+        voiceBtn = (ImageView) v.findViewById(R.id.image_button);
+
+        voiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+        return v;
+    }
+
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "Speak command");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Speech not supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == Activity.RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String voiceData = result.get(0);
+                    voiceData = voiceData.replace(" ","_").toLowerCase();
+                    Toast.makeText(getActivity(), voiceData, Toast.LENGTH_SHORT).show();
+                    runVoiceCommand(voiceData);
+                }
+                break;
+            }
+
+        }
+    }
+
+    public void runVoiceCommand(String command) {
+        HTTPRequestClient.get("/do_action?action="+command,
+                null,
+                new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        //Success code
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        //Failure code
+                        Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+}
